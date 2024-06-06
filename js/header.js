1c4441906880e5b8b4285a9475c1b5269fd741f4
SirajMia <sirajulshuvo08@gmail.com>
2024-06-06 23:06:42 +0600

    add skill section

---
+++ js/header.js
@@ -0,0 +1,14 @@
+export const responsiveHeader = () =>{
+    let startIcon = document.querySelector('.startIcon');
+    console.log(startIcon)
+    let closeIcon = document.querySelector('.closeIcon');
+    let navBar = document.querySelector('.navigation');
+    console.log(navBar)
+    startIcon.addEventListener('click',(e) =>{
+        navBar.classList.add('show');
+        navBar.style.transition = '1s all linear';
+    })
+    closeIcon.addEventListener('click',(e) =>{
+        navBar.classList.remove('show')
+    })
+}
--- js/index.js
+++ js/index.js
@@ -1,21 +1,9 @@
+import { responsiveHeader } from "./header.js";
 import { changeBackground } from "./darkWhite.js";
-changeBackground()
 
-const responsiveHeader = () =>{
-    let startIcon = document.querySelector('.startIcon');
-    console.log(startIcon)
-    let closeIcon = document.querySelector('.closeIcon');
-    let navBar = document.querySelector('.navigation');
-    console.log(navBar)
-    startIcon.addEventListener('click',(e) =>{
-        navBar.classList.add('show');
-        navBar.style.transition = '1s all linear';
-    })
-    closeIcon.addEventListener('click',(e) =>{
-        navBar.classList.remove('show')
-    })
-}
 responsiveHeader();
+changeBackground();
+
 
 
 
