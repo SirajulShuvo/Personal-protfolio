8af1a6c60755e56b4991e3107cf19c26098b5550
SirajMia <sirajulshuvo08@gmail.com>
2024-06-08 22:06:41 +0600

    add localstorage to the dark mood

--- js/darkWhite.js
+++ js/darkWhite.js
@@ -2,39 +2,48 @@ export const changeBackground = () => {
     let listItems = document.querySelectorAll('#list');
     let anchors = document.querySelectorAll('a');
     let body = document.body;
-    let navigation = document.querySelector('.navigation');
     let moon = document.querySelector('#moon');
     let sun = document.querySelector('#sun');
+    function saveData(theme){
+        localStorage.setItem('theme',theme);
+    }
+    const applly = (theme) =>{
+        if(theme === 'dark'){
+            body.style.background = 'black';
+            body.style.color = 'white';
+            anchors.forEach(anchor => {
+                anchor.style.color = 'white';
+            });
+            listItems.forEach(item => {
+                item.style.color = 'white';
+            });
+            moon.style.display = 'none';
+            sun.style.display = 'block';
+        }else{
+            body.style.background = 'white';
+            body.style.color = 'black';
+            anchors.forEach(anchor => {
+                anchor.style.color = 'black';
+            });
+            listItems.forEach(item => {
+                item.style.color = 'black';
+            });
+            moon.style.display = 'block';
+            sun.style.display = 'none';
+        }
+    }
+    const display = () =>{
+        const theme = localStorage.getItem('theme') || 'light';
+        applly(theme)
+    }
     moon.addEventListener('click', (e) => {
-        body.style.background = 'black';
-        body.style.color = 'white';
-        anchors.forEach(anchor => {
-            anchor.style.color = 'white';
-        });
-        listItems.forEach(item => {
-            item.style.color = 'white';
-        });
-        moon.style.display = 'none';
-        sun.style.display = 'block';
+        saveData('dark');
+        applly('dark');
     });
 
     sun.addEventListener('click', (e) => {
-        body.style.background = 'white';
-        body.style.color = 'black';
-        anchors.forEach(anchor => {
-            anchor.style.color = 'black';
-        });
-
-        listItems.forEach(item => {
-            item.style.color = 'black';
-        });
-        moon.style.display = 'block';
-        sun.style.display = 'none';
+        saveData('light')
+       applly('light');
     });
-    function saveData(){
-        
-    }
-    function displayData(){
-        
-    }
+    display();
 };
