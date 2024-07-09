ab68a02e80feb939a85b045ff6147a2fd309d7cd
SirajMia <sirajulshuvo08@gmail.com>
2024-07-09 11:38:53 +0600

    change

--- js/index.js
+++ js/index.js
@@ -2,3 +2,23 @@ import { responsiveHeader } from "./header.js";
 
 
 responsiveHeader();
+
+    document.addEventListener("DOMContentLoaded", function() {
+        let sections = document.querySelectorAll(".section");
+
+        window.addEventListener('scroll', () => {
+            let top = window.scrollY;
+
+            sections.forEach(section => {
+                let offset = section.offsetTop;
+                let offsetHeight = section.offsetHeight;
+                let content = section.querySelector('.section-animation');
+                
+                if (top >= offset && top < offset + offsetHeight) {
+                    content.classList.add('show-animation');
+                } else {
+                    content.classList.remove('show-animation');
+                }
+            });
+        });
+    });
